24d818a976e3724adc54c1caa88fab283f0bd67f
ppxiaobai <309997474@qq.com>
2021-03-25 05:22:56 +0000

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -33,10 +33,11 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
-    public function toDatabase($notifiable){
+    public function toDatabase($notifiable)
+    {
 
         $topic = $this->reply->topic;
         $link = $topic->link(['#reply' . $this->reply->id]);
@@ -62,10 +63,11 @@ class TopicReplied extends Notification
      */
     public function toMail($notifiable)
     {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+            ->line('您的回答有新回复.')
+            ->action('查看回复', $url);
     }
 
     /**
