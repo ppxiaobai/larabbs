5d4330c4a8ed22291b6bd4b1147eeb65df3d94f3
ppxiaobai <309997474@qq.com>
2021-03-25 08:53:13 +0000

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Prophecy\Doubler\Generator\Node\ReturnTypeNode;
 
@@ -18,20 +19,22 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, Topic $topic, User $user)
     {
         $topics = $topic->withOrder($request->order)
             ->with('user', 'category')  // 预加载 N+1 问题
             ->paginate(20);
 
-        return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+
+        return view('topics.index', compact('topics', 'active_users'));
     }
 
     public function show(Request $request, Topic $topic)
     {
         // URL 矫正
-        if (!empty($topic->slug) && $topic->slug != $request->slug){
-           return redirect($topic->link(),301);
+        if (!empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
         }
 
         return view('topics.show', compact('topic'));
