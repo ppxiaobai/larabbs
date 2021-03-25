c77cc7523f01d24f0dfc1952ef2df686efff3f53
ppxiaobai <309997474@qq.com>
2021-03-25 04:04:55 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,19 +3,23 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use PhpParser\Node\Expr\FuncCall;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
-    public function updating(Reply $reply)
+    public function creating(Reply $reply)
     {
-        //
+
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
