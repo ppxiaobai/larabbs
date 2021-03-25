5a902bdb17bd7885f898dc195c399fb30497ce84
ppxiaobai <309997474@qq.com>
2021-03-25 09:48:55 +0000

    用户最后活跃时间

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('inspire')
+        //          ->hourly();
+        // 每隔一个小时执行一遍
+        $schedule->command('larabbs:calculate-active-user')->hourly();
+        // 每日零时执行一次
+        $schedule->command('larabbs:sync-user-actived-at')->dailyAt('00:00');
     }
 
     /**
@@ -34,7 +39,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
