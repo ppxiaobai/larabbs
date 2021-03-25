44dbb548eb69371998be810767d8806404804947
ppxiaobai <309997474@qq.com>
2021-03-25 10:14:31 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // 首页
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -55,3 +55,5 @@ Route::resource('notifications', 'NotificationsController', ['only' => ['index']
 
 // 后台无权限提醒页面
 Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
+
